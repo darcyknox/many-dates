b03ea4f40502c4199b41f274ec49700502b0237f
Darcy Knox <darcy@cloudcannon.com>
2020-04-23 23:00:50 +1200

    Nested error checking

--- ManyDates.java
+++ ManyDates.java
@@ -244,19 +244,23 @@ class ManyDates {
       } else if (days[i] > daysInEachMonth[months[i] - 1]) {
         isValidDate = false;
         errorString = " - INVALID: Day out of range for given month.";
+      } else {
+
+        // adds 2000 to the year if between 0 and 49
+        // adds 1900 to the year if between 50 and 99
+        // invalid date if out of range 1753 - 3000 (both inclusive)
+        if (years[i] >= 0 && years[i] < 50) {
+          years[i] += 2000;
+        } else if (years[i] >= 50 && years[i] < 100) {
+          years[i] += 1900;
+        } else if ((years[i] >= 100 && years[i] < 1753) | years[i] > 3000) {
+          isValidDate = false;
+          errorString = " - INVALID: Year out of range.";
+        }
+
       }
 
-      // adds 2000 to the year if between 0 and 49
-      // adds 1900 to the year if between 50 and 99
-      // invalid date if out of range 1753 - 3000 (both inclusive)
-      if (years[i] >= 0 && years[i] < 50) {
-        years[i] += 2000;
-      } else if (years[i] >= 50 && years[i] < 100) {
-        years[i] += 1900;
-      } else if ((years[i] >= 100 && years[i] < 1753) | years[i] > 3000) {
-        isValidDate = false;
-        errorString = " - INVALID: Year out of range.";
-      }
+
 
       // Output
       String dayString;
